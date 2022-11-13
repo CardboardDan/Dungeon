20708059d9703c0d390466806ca64eb27352ec84
Daniel Korzycki <dan.korzycki@gmail.com>
2022-11-13 19:09:25 +0100

    made begun player

--- player.py
+++ player.py
@@ -2,8 +2,28 @@ import pickle
 import sys
 from definitions import DungeonMap, DungeonRoom
 
+
+def print_exits(map, x, y):
+    room = map.rows[y][x]
+    upward_room = map.rows[y - 1][x]
+    left_room = map.rows[y][x - 1]
+    if room.entry:
+        print("you are at the entrance")
+    if room.exit:
+        print("you are at the exit, you won!!!!!!!!!")
+    if not room.east_wall:
+        print("there  is a passage to the east")
+    if not room.south_wall:
+        print("there is a passage to the south")
+    if not upward_room.south_wall:
+        print("there is a passage to the north")
+    if not left_room.east_wall:
+        print("there is a passage to the west")
+
+
 if __name__ == '__main__':
     filename = sys.argv[1]
     file = open(f'maps\\{filename}.map', 'rb')
     my_map = pickle.load(file)
     my_map.print()
+    print_exits(map=my_map, x=1, y=1)
